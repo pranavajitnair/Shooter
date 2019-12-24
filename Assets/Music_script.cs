65d0ebdf6f918e2c3e8b503f7dc38eeddf8c220c
pranavajitnair <pranavajitnair.cse18@itbhu.ac.in>
2019-12-24 19:26:47 +0530

    plane motion added i guess final

---
+++ Assets/Music_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        Invoke("LoadFirstScene",2);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
