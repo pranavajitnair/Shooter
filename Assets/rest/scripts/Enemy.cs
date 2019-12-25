5fd77c51c7c6f49eae16acbd45ef481644718241
pranavajitnair <pranavajitnair.cse18@itbhu.ac.in>
2019-12-25 20:06:22 +0530

    enemy death added

---
+++ Assets/rest/scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject PrefabToUse;
+
+    void Start()
+    {
+        gameObject.AddComponent<BoxCollider>().isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ParticleSystem sys=Instantiate(PrefabToUse, transform.position, Quaternion.identity).GetComponentInChildren<ParticleSystem>();
+        sys.Play();
+        Destroy(gameObject);
+        print("Collision");
+    }
+}
---
+++ Assets/rest/scripts/ScoreBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score,scoreHit=20;
+    Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText=GetComponent<Text>();
+        scoreText.text=score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ScoreHit(){
+        score+=scoreHit;
+        scoreText.text=score.ToString();
+    }
+}
